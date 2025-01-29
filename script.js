b244f8b331a53437500da1612f69ea1ccb324181
Alberto6126 <alberto.sanchez@enti.cat>
2025-01-29 11:09:27 +0100

    29/01/2025 - Añadido drag

--- script.js
+++ script.js
@@ -54,6 +54,23 @@ function colisiones_huevoMain(){
 
 }
 
+function propiedadesHuevo(huevo){
+huevo.setScale(.75);
+huevo.setInteractive({draggable:true});
+}
+
+function animaHuevos(huevo){
+huevo.angle += rotate;
+//console.log(H_Mondongo.angle)
+
+if (huevo.angle < 40 &&  huevo.angle > -40){
+rotate = rotate;
+}else{
+//console.log(H_Luffy.angle)
+rotate = -rotate;
+}
+
+}
 
 function precarga(){
 this.load.image('FondoBG', 'img/Fondo.webp');
@@ -61,6 +78,7 @@ this.load.image('huevera','img/huevera.png');
 this.load.image('huevoMondongo','img/HuevoMondongo128.png');
 this.load.image('huevoLuffy','img/HuevoLuffy128.png');
 this.load.image('huevoBase','img/HuevoBase128.png');
+this.load.image('trigo', 'img/wheatTEMP.webp')
 }
 
 function crea (){
@@ -69,7 +87,11 @@ function crea (){
 FondoBG = this.add.image(canvas_x/2,canvas_y/2, 'FondoBG');
 FondoBG.setTint(Phaser.Display.Color.GetColor(140,100,90));
 teclas = this.input.keyboard.createCursorKeys();
-
+/*ROTAR IMAGENES O ELEMENTOS EN PHASER CON .angle*/
+//Trigo = this.add.image(canvas_x/2, canvas_y/2, 'trigo');
+//Trigo.angle = 360;
+//Trigo.x = 100;
+//Trigo.y = 100;
 huevera_dragon = this.add.image(150,75, 'huevera');
 huevera_piece = this.add.image(150,225, 'huevera');
 huevera_base = this.add.image(150,375,'huevera');
@@ -83,40 +105,61 @@ huevera_base.setTint(Phaser.Display.Color.GetColor(0,255,0));
 huevera_base.setScale(.75)
 
 H_Mondongo = this.add.image(430 - OffsetX,200, 'huevoMondongo');
-H_Mondongo.setScale(.75);
-H_Mondongo.setInteractive();
-	H_Mondongo.on('pointerdown', () => {
+propiedadesHuevo(H_Mondongo);
+
+H_Mondongo.on('pointerdown', function () {
 	console.log("ClickMondongo")
-	})
+	});
 
 H_Luffy = this.add.image(580 - OffsetX,200, 'huevoLuffy');
-H_Luffy.setScale(.75);
-H_Luffy.setInteractive();
-        H_Luffy.on('pointerdown', () => {
+propiedadesHuevo(H_Luffy);
+      
+			H_Luffy.on('pointerdown', function () {
         console.log("ClickLuffy")
         })
 
 H_Base = this.add.image(720 - OffsetX,200, 'huevoBase');
-H_Base.setScale(.75);
-H_Base.setInteractive();
-        H_Base.on('pointerdown', () => {
+propiedadesHuevo(H_Base);
+
+        H_Base.on('pointerdown', function () {
         console.log("ClickBase")
         })
 
+
+this.input.on('drag', function (pointer, object, x, y){
+	object.x = x;
+	object.y = y;
+	object.setScale(.85);
+});
+this.input.on('dragend', function (pointer, object, x, y){
+	object.setScale(.75);
+});
+
+
 }
+
+let rotate = 1;
 
 function actualiza(){
 // detectarTeclas_huevoMain();
 // colisiones_huevoMain();
 // if(H_Base.y< canvas_y + 70){
-//     H_Base.y +=2;
+  //   H_Base.y +=2;
 // }
 // else if(H_Base.y >= canvas_y){
-//     H_Base.y = -130;
+  //   H_Base.y = -130;
 // }
-    // H_Luffy.y +=1;
-    // H_Mondongo.y +=1;
+   //  H_Luffy.y +=1;
+   //  H_Mondongo.y +=1;
+//H_Mondongo.angle += rotate;
+//console.log(H_Mondongo.angle)
+//if (H_Mondongo.angle >= 40 || H_Mondongo.angle <= -40){
+//rotate = -rotate;
+//}
 
-                
+animaHuevos(H_Mondongo);
+animaHuevos(H_Luffy);
+animaHuevos(H_Base);
+
 }
                 
