931e449a5415c17252fbaaf445c5ded0cf42f558
Alberto Sánchez <alberto.sanchez@enti.cat>
2025-02-05 08:44:54 +0100

    cambios casa

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
+
 let canvas_x = 800;
 let canvas_y = 450;
 let rect_dir = 2;
 let teclas;
 let config = {
-        width: canvas_x,
-        height: canvas_y,
-        scene:{
-                preload: precarga,
-                create: crea,
-                update: actualiza
-        }
+  width: canvas_x,
+  height: canvas_y,
+  scene:{
+    preload: precarga,
+    create: crea,
+    update: actualiza
+  }
 }
 
 let game = new Phaser.Game(config)
 let huevera_dragon, huevera_piece, huevera_base;
-let Huevo;
+// let Huevo;
 let OffsetX = 50;
+let huevoDrag = false;
+//Tendría que ser pública???????? (seguro que no) 
+// let huevoObject={
+//   x,
+//   y,
+//   isDragging,
+//   texturaHuevo
+// };
+//No tengo ni idea de como spawnear más de un huevo a la vez, se porque no se puede pero no se como solucionarlo,
+//he intentado con arrays pero al hacer pop de un objeto y cambiar las posiciones de los valores mantener la lógica se complica mucho (no creo que la solución sea tan compleja)
+//supongo que la solución es hacer un array de objetos con diferentes propiedades (hace mucho más dificil que las cosas cambien cuando se hace un pop ya que casi todas las variables
+//relacionadas al huevo se almacenan dentro del objeto huevo y no de forma externa)
+//aún no se como de buena sea la solución pero no quiero marear el código hasta que haya encontrado como hacerlo de forma correcta
 //GENERO UN ARRAY (EN JS LOS ARRAYS SON DINÁMICOS PUEDO TENER UN ARRAY CON DIFERENTES TIPOS DE DATOS Y CON UNA LONGITUD DINÁMICA , SE ACCEDE DE LA MISMA FORMA QUE EN C++)
-let huevos=['huevoMondongo', 'huevoLuffy', 'huevoBase']
 
+let huevos=['huevoMondongo', 'huevoLuffy', 'huevoBase']
 //NO HE LLEGADO A ENTENDER COMO SE USA TIMER NI DONDE
 // let timer = this.time.addEvent({
 //   delay: 500,
@@ -43,20 +57,50 @@ let huevoSeed = Phaser.Math.Between(0,2);
 let posInicial = Phaser.Math.Between(380,760);
 //TAMBIÉN GENERO UN RÁNDOM PARA COLOCAR LOS HUEVOS EN UN RANGO ENTRE EL 380 Y 760
 
-Huevo = crea.add.image(posInicial, -40, huevos[huevoSeed]);
-Huevo.setScale(.75);
-Huevo.setInteractive({draggable:true});
-console.log(Huevo.texture.key);
+crea.Huevo = crea.add.image(posInicial, -40, huevos[huevoSeed])
+.setScale(.75)
+.setInteractive({draggable:true});
+console.log(crea.Huevo.texture.key);
+
+crea.input.on('drag', function (pointer, object, x, y){
+  crea.Huevo.x = x;
+  crea.Huevo.y = y;
+  crea.Huevo.setScale(.85);
+  huevoDrag = true;
+});
+crea.input.on('dragend', function (pointer, object, x, y){
+  crea.Huevo.setScale(.75);
+  huevoDrag = false;
+});
 
 
-// console.log(huevoSeed);
-// if (Huevo.texture.key === huevos[huevoSeed]){ console.log("SON LO MISMO TIO")} // AHORA SE QUE CON .texture.key puedo acceder a la string de la imagen, por lo tanto puedo identificar a los diferentes huevos
-// Huevo.on('pointerdown', function () {
-//
-//       console.log(""huevos[huevoSeed])
-//       });
+    // // this.input.on('drag', function (pointer, object, x, y){
+    // //     object.x = x;
+    // //     object.y = y;
+    // //     object.setScale(.85);
+    // //     huevoDrag = true;
+    // // });
+    // // this.input.on('dragend', function (pointer, object, x, y){
+    // //     object.setScale(.75);
+    // //     huevoDrag = false;
+    // // });
+
+    // //SOLO ME DEJA LLAMAR A LA FUNCIÓN DE CREAR UNA VEZ, SI LA LLAMO DOS VECES EN EL CREATE, EL HUEVO NO TIENE VELOCIDAD 
+
 }
 
+function creaHueveras(crea){
+    crea.huevera_dragon = crea.add.image(150,75, 'huevera');
+    crea.huevera_piece = crea.add.image(150,225, 'huevera');
+    crea.huevera_base = crea.add.image(150,375,'huevera');
+
+    crea.huevera_dragon.setTint(Phaser.Display.Color.GetColor(0,0,255));
+    crea.huevera_dragon.setScale(.75);
+    crea.huevera_piece.setTint(Phaser.Display.Color.GetColor(255,0,0));
+    crea.huevera_piece.setScale(.75);
+    crea.huevera_base.setTint(Phaser.Display.Color.GetColor(0,255,0));
+    crea.huevera_base.setScale(.75);
+}
 // function animateHuevos(){
 //   let rotationSpeed = 2;
 //   let direction = 1; // 1 para girar a la derecha, -1 para girar a la izquierda
@@ -75,28 +119,7 @@ function crea (){
     FondoBG = this.add.image(canvas_x/2,canvas_y/2, 'FondoBG');
     FondoBG.setTint(Phaser.Display.Color.GetColor(140,100,90));   //USAR setTint(Phaser.Display.Color.GetColor()) PARA CAMBIAR EL TONO DEL SPRITE
     teclas = this.input.keyboard.createCursorKeys();
-
-    huevera_dragon = this.add.image(150,75, 'huevera');
-    huevera_piece = this.add.image(150,225, 'huevera');
-    huevera_base = this.add.image(150,375,'huevera');
-    
-    huevera_dragon.setTint(Phaser.Display.Color.GetColor(0,0,255));
-    huevera_dragon.setScale(.75);
-    huevera_piece.setTint(Phaser.Display.Color.GetColor(255,0,0));
-    huevera_piece.setScale(.75);
-    huevera_base.setTint(Phaser.Display.Color.GetColor(0,255,0));
-    huevera_base.setScale(.75);
-
-    this.input.on('drag', function (pointer, object, x, y){
-        object.x = x;
-        object.y = y;
-        object.setScale(.85);
-    });
-    this.input.on('dragend', function (pointer, object, x, y){
-        object.setScale(.75);
-    });
-
-    //SOLO ME DEJA LLAMAR A LA FUNCIÓN DE CREAR UNA VEZ, SI LA LLAMO DOS VECES EN EL CREATE, EL HUEVO NO TIENE VELOCIDAD 
+    creaHueveras(this)
     crearHuevos(this);
 
 }
@@ -105,26 +128,34 @@ function crea (){
 function actualiza(){
 
 //DIFERENTES VELOCIDADES PARA CADA HUEVO
-  if(Huevo.texture.key == huevos[0]){
-    Huevo.y += 6;
+  if(this.Huevo.texture.key == huevos[0] && !huevoDrag){
+    this.Huevo.y += 6;
   }
-  if(Huevo.texture.key == huevos[1]){
-    Huevo.y += 4;
+  if(this.Huevo.texture.key == huevos[1] && !huevoDrag){
+    this.Huevo.y += 4;
   }
-  if(Huevo.texture.key == huevos[2]){
-    Huevo.y += 3;
+  if(this.Huevo.texture.key == huevos[2] && !huevoDrag){
+    this.Huevo.y += 3;
   }
 
   // animateHuevos();
 
-  if(Huevo.y >= canvas_y + 70){
+  if(this.Huevo.y >= canvas_y + 70){
     //PHASER TIENE UNA FUNCIÓN PARA DESTRUIR OBJETOS, PERO HAY GENTE QUE PREFIERE DESACTIVARLOS EN PANTALLA PARA PODER SEGUIR ACCEDIENDO A SU INFORMACIÓN, COMO NO ES NUESTRO CASO, USO DESTROY()
-      Huevo.destroy();
+    this.Huevo.destroy();
       //CADA VEZ QUE UN HUEVO ES DESTRUIDO CREO OTRO
       crearHuevos(this);
 
     }
 
-  
+    if (Phaser.Geom.Intersects.RectangleToRectangle(this.huevera_dragon.getBounds(), this.Huevo.getBounds())){
+      console.log("hueveraDragon");
+    }
+    else if (Phaser.Geom.Intersects.RectangleToRectangle(this.huevera_piece.getBounds(), this.Huevo.getBounds())){
+      console.log("hueveraPiece");
+    }
+    else if (Phaser.Geom.Intersects.RectangleToRectangle(this.huevera_base.getBounds(), this.Huevo.getBounds())){
+      console.log("hueveraBase");
+    }
 
 }
